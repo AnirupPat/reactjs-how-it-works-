4bee3307552d4822459e4bf1a966b128c206056a
Anirup Patnaik <anirup049@gmail.com>
2021-06-01 11:38:53 +0530

    Initial Setup
    How a new element renders in Real DOM

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import Button from './components/UI/Button/Button';
 import './App.css';
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+  console.log('Component re-rendered!')
+  const handleButtonClick = () => {
+    setShowParagraph(prevState => !prevState);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Hi There!</h1>
+      {showParagraph && <p>This is brand new</p>}
+      <Button onClick={handleButtonClick}>Click</Button>
     </div>
   );
 }
---
+++ src/components/Demo/DemoOutput.js
@@ -0,0 +1,9 @@
+
+
+const DemoOutput = props => {
+    // return (
+
+    // );
+}
+
+export default DemoOutput;
